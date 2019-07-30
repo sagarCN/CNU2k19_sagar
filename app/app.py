dfa7a8412a1c892fe1ef91c354b2cc9961333128
Sagar Gupta <sagar@Sagars-MacBook-Pro.local>
2019-07-30 12:33:32 +0400

    Update readme

---
+++ app/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import flask
+import redis
+import os
+
+
+app = Flask(__name__)
+app.debug = True
+
+bind_port = os.environ['BIND_PORT']
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = os.environ['REDIS_PORT']
+
+
+db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT) 
+
+
+@app.route("/")
+def index():
+	if db.exists("3"):
+		db.mset({"1":int(db.get("2"))})
+		db.mset({"2":int(db.get("3"))})
+		db.mset({"3":int(db.get("1")) + int(db.get("2"))})
+	elif db.exists("2"):
+		db.mset({"3":int(db.get("1")) + int(db.get("2"))})
+	elif db.exists("1"):
+		db.mset({"2":1})
+		return "Reloaded %d number of times" % int(db.get("2"))
+	else:
+		db.mset({"1":1})
+		return "Reloaded %d number of times" % int(db.get("1"))
+
+	return "Reloaded %d number of times" % int(db.get("3"))
+
+
+if __name__ == '__main__':
+	app.run(debug=False, host='0.0.0.0')
